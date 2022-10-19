f48e544b2cb6ee0ba0a4e211b3e856bbdaf0f18b
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2022-10-19 00:18:55 -0500

    Code Refactored - Code Styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -20,12 +20,29 @@ function Footer(): React.ReactElement {
   return (
     <Box sx={styles.footerWrapper}>
       <Typography align="center" variant="body2" sx={styles.text}>
-        Copyright © 2019 By <Link href="https://hcjang.com">Hyecheol (Jerry) Jang</Link> & <Link href="https://pages.cs.wisc.edu/~linn/WaiZinPage/">Wai Zin Linn</Link>.
+        Copyright © 2019 By{' '}
+        <Link href="https://hcjang.com">Hyecheol (Jerry) Jang</Link> &{' '}
+        <Link href="https://pages.cs.wisc.edu/~linn/WaiZinPage/">
+          Wai Zin Linn
+        </Link>
+        .
       </Typography>
-      <Typography align="center" variant="body2" sx={{ ...styles.boldText, ...styles.text }}>
-        THIS IS NOT A REAL WEBSITE TO PURCHASE THE TICKETS FOR WISCONSIN BADGERS GAMES.
+      <Typography
+        align="center"
+        variant="body2"
+        sx={{ ...styles.boldText, ...styles.text }}
+      >
+        THIS IS NOT A REAL WEBSITE TO PURCHASE THE TICKETS FOR WISCONSIN BADGERS
+        GAMES.
       </Typography>
-      <Link align="center" href="/terms" variant="caption" sx={{ ...styles.termsLink, ...styles.text }}>Terms and Condition</Link>
+      <Link
+        align="center"
+        href="/terms"
+        variant="caption"
+        sx={{ ...styles.termsLink, ...styles.text }}
+      >
+        Terms and Condition
+      </Link>
     </Box>
   );
 }
