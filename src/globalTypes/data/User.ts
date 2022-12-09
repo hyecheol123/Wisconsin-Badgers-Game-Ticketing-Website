1ac48c52f379f363f22db56a83772e740e881350
Hyecheol (Jerry) Jang <hyecheol.jang@wisc.edu>
2022-12-08 21:10:53 -0600

    Purchase Modal needs Information of current Sign-in User

--- src/globalTypes/data/User.ts
+++ src/globalTypes/data/User.ts
@@ -7,7 +7,7 @@
 
 // Google Firebase
 import { FirebaseApp } from 'firebase/app';
-import { doc, getFirestore, setDoc } from 'firebase/firestore';
+import { doc, getDoc, getFirestore, setDoc } from 'firebase/firestore';
 
 export type User = {
   email: string;
@@ -23,4 +23,26 @@ export type User = {
  */
 export async function createUser(app: FirebaseApp, data: User): Promise<void> {
   return setDoc(doc(getFirestore(app), 'user', data.email), data);
+}
+
+/**
+ * Function to get User detail by user's email
+ *
+ * @param {FirebaseApp} app firebase application associated with the project
+ * @param {string} email email is used as unique id
+ * @return {Promise<User | undefined>} Promise which will be resolved to either User or undefined (not exist).
+ */
+export async function getUserByEmail(
+  app: FirebaseApp,
+  email: string
+): Promise<User | undefined> {
+  const snapshot = await getDoc(doc(getFirestore(app), 'user', email));
+  if (snapshot.exists()) {
+    const result = snapshot.data() as User;
+    result['email'] = snapshot.id;
+
+    return result;
+  } else {
+    return undefined;
+  }
 }
